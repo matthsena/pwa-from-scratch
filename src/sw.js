733d816eac33eb2198f84412b346b318034e01c7
Matheus <matheusalexandre.sena99@gmail.com>
2020-07-26 17:56:55 -0300

    Caching images and assets

--- src/sw.js
+++ src/sw.js
@@ -1,11 +1,21 @@
 const CACHE_NAME = 'pwa-cache-v1';
 const urlsToCache = [
+    './',
     './index.html',
-    './manifest.json'
+    './assets/css/style.css',
+    './assets/js/app.js',
+    './manifest.json',
+    './img/barcat-72x72.png',
+    './img/barcat-96x96.png',
+    './img/barcat-128x128.png',
+    './img/barcat-144x144.png',
+    './img/barcat-152x152.png',
+    './img/barcat-192x192.png',
+    './img/barcat-384x384.png',
+    './img/barcat-512x512.png'
 ]
 
 self.addEventListener('install', (event) => {
-
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             console.info('opened cache')
@@ -15,10 +25,8 @@ self.addEventListener('install', (event) => {
 })
 
 self.addEventListener('activate', (event) => {
-
     event.waitUntil(
         caches.keys().then((keys) => {
-
             return Promise.all(
                 keys.filter((key) => {
                     return key.indexOf(CACHE_NAME) !== 0
